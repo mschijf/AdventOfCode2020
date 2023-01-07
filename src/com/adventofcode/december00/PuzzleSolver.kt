f7a7645582f33d4956ed80036a28dfdd87b83eea
mschijf <mschijf@gmail.com>
2023-01-07 18:50:32 +0100

    December 00 - template (easy to copy)

---
+++ src/com/adventofcode/december00/PuzzleSolver.kt
@@ -0,0 +1,13 @@
+package com.adventofcode.december00
+
+import com.adventofcode.PuzzleSolverAbstract
+
+fun main() {
+    PuzzleSolver(test=false).showResult()
+}
+
+class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
+
+}
+
+
