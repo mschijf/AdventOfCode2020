84e3098a086b821ff3f54fec0fac141ef5da2a26
mschijf <mschijf@gmail.com>
2023-01-07 09:08:02 +0100

    Made Grid and Pos classes for solution december03

--- src/com/adventofcode/december03/PuzzleSolver.kt
+++ src/com/adventofcode/december03/PuzzleSolver.kt
@@ -8,33 +8,51 @@ fun main() {
 
 class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
 
-    private val forest = input.inputLines
+    private val forest = Grid(input.inputLines.map {it.toList()})
 
     override fun resultPartOne(): String {
-        return countTrees(Pair(1,3)).toString()
+        return countTrees(Pos(3,1)).toString()
     }
 
     override fun resultPartTwo(): String {
         var result = 1L
-        val slopeList = listOf(Pair(1,1), Pair(1,3), Pair(1,5), Pair(1,7), Pair(2,1))
+        val slopeList = listOf(Pos(1,1), Pos(3,1), Pos(5,1), Pos(7,1), Pos(1,2))
         for (slope in slopeList) {
             result *= countTrees(slope)
         }
         return result.toString()
     }
 
-    private fun countTrees(slope: Pair<Int, Int>): Int {
-        var x = 0
-        var y = 0
+
+    private fun countTrees(slope: Pos): Int {
+        var pos = Pos(0,0)
         var countTree = 0
-        while (y < forest.size) {
-            if (forest[y][x % forest[y].length] == '#')
+        while (forest.containsPos(pos)) {
+            if (forest[pos] == '#')
                 countTree++
-            x += slope.second
-            y += slope.first
+            pos = (pos + slope).modX(forest.maxX)
         }
+        println("$slope $countTree")
         return countTree
     }
+}
+
+class Grid<T> (
+    private val input: List<List<T>>){
+
+    val maxY = input.size
+    val maxX = input[0].size
+
+    fun containsPos(pos:Pos) = pos.x in 0 until maxX && pos.y in 0 until maxY
+
+    operator fun get(pos: Pos) = input[pos.y][pos.x]
+}
+
+data class Pos(val x: Int, val y: Int) {
+    operator fun plus(pos: Pos) = Pos(x+pos.x, y+pos.y)
+    operator fun rem(denominator: Pos) = Pos(x % denominator.x,y % denominator.y)
+    fun modX(denominatorX: Int) = Pos(x % denominatorX, y)
+    fun modY(denominatorY: Int) = Pos(x, y % denominatorY)
 }
 
 
